f8d8567a5dc7da880b5e2eaf827a2f434339c063
sofronijev <sofronijev@gmail.com>
2025-10-19 10:58:56 +0200

    Initial changes to main screen, add multiLine to textInput, set theme colors

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,20 @@
+import { Colors } from '@/constants/Colors';
 import { generateAPIUrl } from '@/utils';
 import { useChat } from '@ai-sdk/react';
 import { DefaultChatTransport } from 'ai';
 import { fetch as expoFetch } from 'expo/fetch';
 import { useState } from 'react';
-import { View, TextInput, ScrollView, Text, SafeAreaView } from 'react-native';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   const [input, setInput] = useState('');
@@ -14,31 +25,43 @@ export default function App() {
     }),
     onError: error => console.error(error, 'ERROR'),
   });
+  const colorScheme = useColorScheme() ?? 'light';
+  const tColors = Colors[colorScheme];
 
   if (error) return <Text>{error.message}</Text>;
 
   return (
-    <SafeAreaView style={{ height: '100%' }}>
-      <View
-        style={{
-          height: '95%',
-          display: 'flex',
-          flexDirection: 'column',
-          paddingHorizontal: 8,
-        }}
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: tColors.background }]}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.flex}
       >
-        <ScrollView style={{ flex: 1 }}>
+        <ScrollView showsVerticalScrollIndicator={false} style={styles.flex}>
           {messages.map(m => (
-            <View key={m.id} style={{ marginVertical: 8 }}>
+            <View key={m.id} style={styles.msgContainer}>
               <View>
-                <Text style={{ fontWeight: 700 }}>{m.role}</Text>
+                <Text style={[styles.msgText, { color: tColors.text }]}>
+                  {m.role}
+                </Text>
                 {m.parts.map((part, i) => {
                   switch (part.type) {
                     case 'text':
-                      return <Text key={`${m.id}-${i}`}>{part.text}</Text>;
+                      return (
+                        <Text
+                          style={{ color: tColors.text }}
+                          key={`${m.id}-${i}`}
+                        >
+                          {part.text}
+                        </Text>
+                      );
                     case 'tool-weather':
                       return (
-                        <Text key={`${m.id}-${i}`}>
+                        <Text
+                          key={`${m.id}-${i}`}
+                          style={{ color: tColors.text }}
+                        >
                           {JSON.stringify(part, null, 2)}
                         </Text>
                       );
@@ -48,12 +71,16 @@ export default function App() {
             </View>
           ))}
         </ScrollView>
-        <View style={{ marginTop: 8 }}>
+        <View style={styles.inputContainer}>
           <TextInput
-            style={{ backgroundColor: 'white', padding: 8 }}
+            style={[
+              styles.inputText,
+              { color: tColors.text, backgroundColor: tColors.greyBackground },
+            ]}
             placeholder="Say something..."
+            multiline
             value={input}
-            onChange={e => setInput(e.nativeEvent.text)}
+            onChangeText={setInput}
             onSubmitEditing={e => {
               e.preventDefault();
               sendMessage({ text: input });
@@ -62,7 +89,23 @@ export default function App() {
             autoFocus={true}
           />
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  flex: { flex: 1 },
+  msgContainer: { marginVertical: 8 },
+  msgText: { fontWeight: 700 },
+  inputContainer: { marginTop: 8 },
+  inputText: {
+    padding: 12,
+    marginBottom: 8,
+    borderRadius: 24,
+  },
+});
